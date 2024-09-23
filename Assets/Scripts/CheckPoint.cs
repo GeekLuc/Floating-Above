05888c390d764815cc231974c75fa2fb1e53b762
GeekLuc <luca.rougefort@student.heaj.be>
2024-09-23 14:29:18 +0200

    polish checkpoint + bar ballon

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,6 +6,20 @@ public class CheckPoint : MonoBehaviour
 {
     private bool isUnlocked = false;
     [SerializeField] ParticleSystem _vfx;
+    [SerializeField] Renderer Drapeau; 
+
+    private Color _defaultColor1;
+    private Color _defaultColor2;
+    private Color _defaultColor3;
+    private Color _defaultColor4;
+
+    private void Start()
+    {
+        // Sauvegarder les couleurs par défaut
+        if (Drapeau != null) _defaultColor1 = Drapeau.material.color;
+
+        UpdateCheckpointColor();
+    }
 
     public void CheckIfPlayerPassed(GameObject player)
     {
@@ -25,6 +39,7 @@ public class CheckPoint : MonoBehaviour
         isUnlocked = true;
         PlayCheckpointUnlockSound();
         PlayCheckpointUnlockVFX();
+        UpdateCheckpointColor();
     }
 
     public bool IsUnlocked()
@@ -46,5 +61,15 @@ public class CheckPoint : MonoBehaviour
     private void PlayCheckpointUnlockVFX()
     {
         _vfx.Play();
+    }
+
+    private void UpdateCheckpointColor()
+    {
+        Color targetColor = isUnlocked ? Color.white : Color.black;
+
+        if (Drapeau != null)
+        {
+            Drapeau.material.color = isUnlocked ? _defaultColor1 : targetColor;
+        }
     }
 }
